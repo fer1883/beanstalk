23941d13f3ab9a9570d1dc0fece33b4bf2130185
Fernando Correa <fernando@diwit.net>
2021-09-15 16:38:24 -0300

    cambio de comando

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 
 const port = process.env.port || 3000
+
 app.get('/', (req, res) => {
-    res.send(' Todos TERMO')
+    res.send(' Todos TERMO' + process.env.termo)
 })
 
 app.listen(port, () => {
