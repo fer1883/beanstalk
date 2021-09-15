7d11519b2e1f8056316935a961b445a6305f0c20
Fernando Correa <fernando@diwit.net>
2021-09-15 15:29:56 -0300

    cambio en el puerto

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.port || 3000
 app.get('/', (req, res) => {
     res.send('bienvenido a la home')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('que onda!')
 })
